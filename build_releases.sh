2e2b41f72fa836d7f3d1471adada50266d11f3fe
Patrick Chan <patrickchan1999@gmail.com>
2019-10-14 23:25:13 -0400

    Add a script for building release artifacts

---
+++ build_releases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo 'Generating binary for linux...'
+gox -osarch="linux/amd64"
+mv pinger_linux_amd64 pinger
+tar -czf pinger-0.0.1-x86_64-linux.tar.gz pinger
+
+echo 'Generating binary for darwin...'
+gox -osarch="darwin/amd64"
+mv pinger_darwin_amd64 pinger
+tar -czf pinger-0.0.1-x86_64-darwin.tar.gz pinger
+
+echo 'Generating binary for windows...'
+gox -osarch="windows/amd64"
+mv pinger_windows_amd64.exe pinger.exe
+zip pinger-0.0.1-x86_64-windows.zip pinger.exe
+
+echo 'Cleaning up...'
+rm pinger pinger.exe
