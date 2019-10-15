19e0095dd0f9573d319a30fb93f7704149d0d306
Patrick Chan <patrickchan1999@gmail.com>
2019-10-14 22:26:49 -0400

    Finish initial version
    Pings google.com to check for internet access to outside services.  Pings blackboard.stonybrook.edu
    if that fails to check if we at least have internet access to internal services.  Log each ping
    attempt.
    
    Note: Currently require to be ran as root on Unix machines.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/sbu-ces-unofficial/pinger
+
+go 1.13
+
+require (
+	github.com/PGo-Projects/output v0.0.0-20190826023914-73e670db3489
+	github.com/glinton/ping v0.1.2
+	github.com/google/logger v1.0.1
+	github.com/robfig/cron/v3 v3.0.0
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/PGo-Projects/output"
+	"github.com/glinton/ping"
+	"github.com/google/logger"
+	"github.com/robfig/cron/v3"
+)
+
+const logPath = "internet_connectivity.log"
+
+func pingServer() {
+	_, err := ping.IPv4(context.Background(), "google.com")
+	if err != nil {
+		_, err = ping.IPv4(context.Background(), "blackboard.stonybrook.edu")
+		if err != nil {
+			logger.Errorln("Pinging google.com and blackboard.stonybrook.edu failed")
+		} else {
+			logger.Warningln("Pinging google.com failed, but pinging blackboard.stonybrook.edu succeeded")
+		}
+	} else {
+		logger.Infoln("Pinging google.com succeeded")
+	}
+}
+
+func main() {
+	_, err := ping.IPv4(context.Background(), "google.com")
+	if err != nil && strings.Contains(err.Error(), "socket: permission denied") {
+		output.ErrorStringln("This program requires root access on Unix machines to send ICMP packets.")
+		os.Exit(1)
+	}
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+	defer logger.Init("internet_connectivity", false, false, logFile).Close()
+
+	c := cron.New()
+	c.AddFunc("* * * * *", pingServer)
+	c.Start()
+
+	output.Println("Pinging servers...  Please note that the first ping would start at the minute mark.", output.GREEN)
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		_ = <-sigs
+		done <- true
+	}()
+
+	<-done
+}
