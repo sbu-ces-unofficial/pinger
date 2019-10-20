5573dd3610c1bb64eae467f68926bd3bb952366d
Patrick Chan <patrickchan1999@gmail.com>
2019-10-20 13:37:05 -0400

    Fixes #2

---
+++ cmd/pinger.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/PGo-Projects/output"
+	"github.com/glinton/ping"
+	"github.com/google/logger"
+	"github.com/robfig/cron/v3"
+	"github.com/spf13/cobra"
+)
+
+const logPath = "internet_connectivity.log"
+
+var pingerCmd = &cobra.Command{
+	Use:   "pinger",
+	Short: "Pinger is an utility used to collect more data concerning network outages",
+	Long:  `A utility to help determine when network outage occurs and how much of the network is down.`,
+	Run:   pinger,
+}
+
+func pingServer() {
+	_, err := ping.IPv4(context.Background(), "google.com")
+	if err != nil {
+		_, err = ping.IPv4(context.Background(), "blackboard.stonybrook.edu")
+		if err != nil {
+			logger.Errorln("Pinging google.com and blackboard.stonybrook.edu failed")
+		} else {
+			logger.Warningln("Pinging google.com failed, but pinging blackboard.stonybrook.edu succeeded")
+		}
+	} else {
+		logger.Infoln("Pinging google.com succeeded")
+	}
+}
+
+func pinger(cmd *cobra.Command, args []string) {
+	_, err := ping.IPv4(context.Background(), "google.com")
+	if err != nil && strings.Contains(err.Error(), "socket: permission denied") {
+		output.ErrorStringln("This program requires root access on Unix machines to send ICMP packets.")
+		os.Exit(1)
+	}
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+	defer logger.Init("internet_connectivity", false, false, logFile).Close()
+	logger.SetFlags(log.Ldate | log.Lmicroseconds)
+
+	c := cron.New()
+	c.AddFunc("* * * * *", pingServer)
+	c.Start()
+
+	output.Println("Pinging servers...  Please note that the first ping would start at the minute mark.", output.GREEN)
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		_ = <-sigs
+		done <- true
+	}()
+
+	<-done
+}
+
+func Execute() {
+	if err := pingerCmd.Execute(); err != nil {
+		output.Errorln(err)
+		os.Exit(1)
+	}
+}
