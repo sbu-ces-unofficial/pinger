813f1a8bf8a8a1c4f41b44e3f4fabbae63d29faf
Patrick Chan <patrickchan1999@gmail.com>
2019-10-21 00:43:51 -0400

    Quick hack to address #4

--- cmd/pinger.go
+++ cmd/pinger.go
@@ -12,30 +12,46 @@ import (
 	"github.com/glinton/ping"
 	"github.com/google/logger"
 	"github.com/robfig/cron/v3"
+	"github.com/sbu-ces-unofficial/pinger/internal/pflags"
 	"github.com/spf13/cobra"
 )
 
 const logPath = "internet_connectivity.log"
 
+var (
+	externalURLs = []string{"google.com"}
+	internalURLs = []string{"blackboard.stonybrook.edu"}
+)
+
 var pingerCmd = &cobra.Command{
 	Use:     "pinger",
 	Short:   "Pinger is an utility used to collect more data concerning network outages",
 	Long:    `A utility to help determine when network outage occurs and how much of the network is down.`,
-	Version: "0.0.2",
+	Version: "0.0.3a",
 	Run:     pinger,
 }
 
 func pingServer() {
-	_, err := ping.IPv4(context.Background(), "google.com")
-	if err != nil {
-		_, err = ping.IPv4(context.Background(), "blackboard.stonybrook.edu")
+	var externalPingErr error = nil
+	for _, url := range externalURLs {
+		_, err := ping.IPv4(context.Background(), url)
 		if err != nil {
-			logger.Errorln("Pinging google.com and blackboard.stonybrook.edu failed")
+			externalPingErr = err
+			logger.Warningf("Pinging %s failed\n", url)
 		} else {
-			logger.Warningln("Pinging google.com failed, but pinging blackboard.stonybrook.edu succeeded")
+			logger.Infof("Pinging %s succeeded\n", url)
 		}
-	} else {
-		logger.Infoln("Pinging google.com succeeded")
+	}
+
+	if externalPingErr != nil {
+		for _, url := range internalURLs {
+			_, err := ping.IPv4(context.Background(), url)
+			if err != nil {
+				logger.Errorf("Pinging %s failed\n", url)
+			} else {
+				logger.Warningf("Pinging %s succeeded\n", url)
+			}
+		}
 	}
 }
 
@@ -46,13 +62,20 @@ func pinger(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	config, err := pflags.Parse("config.pflags")
+	if err == nil {
+		output.Successln("Using configuration specified in config.pflags!")
+		externalURLs = config.Arrays["external_urls"]
+		internalURLs = config.Arrays["internal_urls"]
+	}
+
 	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
 	if err != nil {
 		panic(err)
 	}
 
 	defer logFile.Close()
-	defer logger.Init("internet_connectivity", false, false, logFile).Close()
+	defer logger.Init("internet_connectivity", true, false, logFile).Close()
 	logger.SetFlags(log.Ldate | log.Lmicroseconds)
 
 	c := cron.New()
